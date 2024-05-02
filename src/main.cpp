bb38f23e7f03a6e40435b7301c507fc8599e105a
ashraf <100951343+makronomic@users.noreply.github.com>
2024-05-02 18:33:49 +0300

    Moved the files in their correct locations Adjusted include directories

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include "../MainMenu.h"
+#include "MainMenu.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 
 //helper function during development, returns mouse position in the window when you click anywhere 
-void mousePos(sf::RenderWindow& window)
-{
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-	{
+void mousePos(sf::RenderWindow& window) {
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 		sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
 		std::cout << "X: " << mousePosition.x << " Y: " << mousePosition.y << std::endl;
 	}
@@ -20,11 +18,9 @@ int main() {
 	int chosenLevel = -1;
 	int chosenDifficulty = -1;
 
-	while (window.isOpen())
-	{
+	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
 			mousePos(window);
 
 			if (event.type == sf::Event::Closed)
@@ -32,18 +28,15 @@ int main() {
 		}
 
 		window.clear();
-		
-		if (mainMenu.getChosenDifficulty() != 0 && mainMenu.getChosenLevel() != 0)
-		{
-			
+
+		if (mainMenu.getChosenDifficulty() != 0 && mainMenu.getChosenLevel() != 0) {
+
 			std::cout << "LEVEL: " << mainMenu.getChosenLevel() << " DIFFICULTY: " << mainMenu.getChosenDifficulty() << std::endl;
 			//run game with parameters chosen level and chosen difficulty
 			//gamelogic(level,difficulty)
 			mainMenu.stopMusic();
 			currentMenuState = GameLogicState;
-		}
-		else if(currentMenuState != GameLogicState)
-		{
+		} else if (currentMenuState != GameLogicState) {
 			mainMenu.drawMenu(window);
 		}
 
